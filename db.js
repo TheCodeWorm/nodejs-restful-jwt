a3b67a2eeb8358dc5448f78f4efd0a883ae72fb7
unknown <4purposeliving@gmail.com>
2018-07-01 19:50:39 -0700

    added server.js

--- db.js
+++ db.js
@@ -1,1 +1,3 @@
-const mongoose = require('mongoose');
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://thecodeworm:password0123@ds123981.mlab.com:23981/securing-nodejs', { useMongoClient: true });
