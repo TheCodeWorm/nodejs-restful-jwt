cb46dbcbcaaeacc5190350c4780e58441a4179ba
unknown <4purposeliving@gmail.com>
2018-07-02 07:51:54 -0700

    connect db

--- db.js
+++ db.js
@@ -1,6 +1,16 @@
 var mongoose = require('mongoose');
 
-//mongoose.connect('mongodb://thecodeworm:password0123@ds123981.mlab.com:23981/securing-nodejs', { useMongoClient: true });
+const uri = 'mongodb://user:password123@ds123981.mlab.com:23981/securing-nodejs';
 
-// The `useMongoClient` option is no longer necessary in mongoose 5.x, please remove it.
-mongoose.connect('mongodb://thecodeworm:password0123@ds123981.mlab.com:23981/securing-nodejs');
+//Set up mongoose connection
+mongoose.connect(uri);
+
+// Get Mongoose to use the global promise library
+mongoose.Promise = global.Promise;
+
+//Get the default connection
+var db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
