36da8af9e281d42be6454e6c37ab913da1f9ec06
unknown <4purposeliving@gmail.com>
2018-07-02 08:13:03 -0700

    config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+
+// NEVER have your secret keys visible 
+// place your keys in env variables
+// this is only for demo purposes
+
+
+module.exports = {
+  'secret': 'supersecret'
+};
--- db.js
+++ db.js
@@ -2,15 +2,19 @@ var mongoose = require('mongoose');
 
 const uri = 'mongodb://user:password123@ds123981.mlab.com:23981/securing-nodejs';
 
+mongoose.connect(uri);
+
+/*
 //Set up mongoose connection
 mongoose.connect(uri);
 
 // Get Mongoose to use the global promise library
 mongoose.Promise = global.Promise;
 
-//Get the default connection
+//Get connection
 var db = mongoose.connection;
 
 //Bind connection to error event (to get notification of connection errors)
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+*/
 
