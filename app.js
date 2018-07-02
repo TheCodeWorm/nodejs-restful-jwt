ddeaa195bf4bfc693114083cda384d0e26a66fd4
unknown <4purposeliving@gmail.com>
2018-07-01 20:17:59 -0700

    fixed deprecated app.router

--- app.js
+++ app.js
@@ -3,6 +3,5 @@ const app = express();
 const db = require('./db');
 
 const UserController = require('./user/UserController');
-app.use('/users', UserController);
 
 module.exports = app;
--- db.js
+++ db.js
@@ -1,3 +1,6 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://thecodeworm:password0123@ds123981.mlab.com:23981/securing-nodejs', { useMongoClient: true });
+//mongoose.connect('mongodb://thecodeworm:password0123@ds123981.mlab.com:23981/securing-nodejs', { useMongoClient: true });
+
+// The `useMongoClient` option is no longer necessary in mongoose 5.x, please remove it.
+mongoose.connect('mongodb://thecodeworm:password0123@ds123981.mlab.com:23981/securing-nodejs');
