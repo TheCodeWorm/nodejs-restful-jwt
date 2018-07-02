b89cff10eaecd0d4fa0f3636445c93d4aaa49743
unknown <4purposeliving@gmail.com>
2018-07-02 10:58:22 -0700

    auth

--- app.js
+++ app.js
@@ -4,4 +4,7 @@ const db = require('./db');
 
 const UserController = require('./user/UserController');
 
+const AuthController = require('./auth/AuthController');
+app.use('/api/auth', AuthController);
+
 module.exports = app;
