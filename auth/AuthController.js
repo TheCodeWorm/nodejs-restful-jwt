3fc7db5704ff598b0ba935d878e6328a40e00131
unknown <4purposeliving@gmail.com>
2018-07-06 18:43:41 -0700

    next

--- auth/AuthController.js
+++ auth/AuthController.js
@@ -48,7 +48,10 @@ router.get('/me', function(req, res) {
     		return res.status(500).send("There was a problem finding the user. ");
     	if (!user) 
     		return res.status(404).send("No user found. ");
-    	res.status(200).send(user);
+
+      // following line is same as the one after that
+    	/* res.status(200).send(user); */
+      next(user);
 	});
   });
 });
--- config.js
+++ config.js
@@ -5,5 +5,5 @@
 
 
 module.exports = {
-  'secret': 'supersecret'
+  'secret_key': 'supersecret'
 };
